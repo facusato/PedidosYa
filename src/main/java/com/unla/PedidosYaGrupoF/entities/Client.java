a2ea564de8cc87e5efb630b3cb1326be333ada14
unknown <facundocusato12@gmail.com>
2021-08-31 17:01:51 -0300

    Agregando Personas

---
+++ src/main/java/com/unla/PedidosYaGrupoF/entities/Client.java
@@ -0,0 +1,28 @@
+package com.unla.PedidosYaGrupoF.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Client extends Person {
+	@Column(name="mail")
+	private String mail;
+	
+	public Client() {}
+	
+	public Client(long id,String name,String surname,LocalDate dateBirth,long dni, String mail) {
+		super(id,name,surname,dateBirth,dni);
+		this.mail = mail;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+}
